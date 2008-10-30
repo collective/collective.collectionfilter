539d0fe3fab077ae757d3b02dd59f6cc354785d4
Rok Garbas <rok.garbas@gmail.com>
2008-10-30 02:44:27 +0100

    Initial commit

---
+++ collective/portlet/collectionbysubject/tests/test_portlet.py
@@ -0,0 +1,109 @@
+from zope.component import getUtility, getMultiAdapter
+
+from plone.portlets.interfaces import IPortletType
+from plone.portlets.interfaces import IPortletManager
+from plone.portlets.interfaces import IPortletAssignment
+from plone.portlets.interfaces import IPortletDataProvider
+from plone.portlets.interfaces import IPortletRenderer
+
+from plone.app.portlets.storage import PortletAssignmentMapping
+
+from collective.portlet.collectionbysubject import collectionbysubject
+
+from collective.portlet.collectionbysubject.tests.base import TestCase
+
+
+class TestPortlet(TestCase):
+
+    def afterSetUp(self):
+        self.setRoles(('Manager', ))
+
+    def test_portlet_type_registered(self):
+        portlet = getUtility(
+            IPortletType,
+            name='collective.portlet.collectionbysubject.CollectionBySubject')
+        self.assertEquals(portlet.addview,
+                          'collective.portlet.collectionbysubject.CollectionBySubject')
+
+    def test_interfaces(self):
+        portlet = collectionbysubject.Assignment(header='test header')
+        self.failUnless(IPortletAssignment.providedBy(portlet))
+        self.failUnless(IPortletDataProvider.providedBy(portlet.data))
+
+    def test_invoke_add_view(self):
+        portlet = getUtility(
+            IPortletType,
+            name='collective.portlet.collectionbysubject.CollectionBySubject')
+        mapping = self.portal.restrictedTraverse(
+            '++contextportlets++plone.leftcolumn')
+        for m in mapping.keys():
+            del mapping[m]
+        addview = mapping.restrictedTraverse('+/' + portlet.addview)
+
+        # TODO: Pass a dictionary containing dummy form inputs from the add
+        # form.
+        addview.createAndAdd(data={})
+
+        self.assertEquals(len(mapping), 1)
+        self.failUnless(isinstance(mapping.values()[0],
+                                   collectionbysubject.Assignment))
+
+    def test_invoke_edit_view(self):
+        # NOTE: This test can be removed if the portlet has no edit form
+        mapping = PortletAssignmentMapping()
+        request = self.folder.REQUEST
+
+        mapping['foo'] = collectionbysubject.Assignment()
+        editview = getMultiAdapter((mapping['foo'], request), name='edit')
+        self.failUnless(isinstance(editview, collectionbysubject.EditForm))
+
+    def test_obtain_renderer(self):
+        context = self.folder
+        request = self.folder.REQUEST
+        view = self.folder.restrictedTraverse('@@plone')
+        manager = getUtility(IPortletManager, name='plone.rightcolumn',
+                             context=self.portal)
+
+        assignment = collectionbysubject.Assignment(header='test header')
+
+        renderer = getMultiAdapter(
+            (context, request, view, manager, assignment), IPortletRenderer)
+        self.failUnless(isinstance(renderer, collectionbysubject.Renderer))
+
+
+class TestRenderer(TestCase):
+
+    def afterSetUp(self):
+        self.setRoles(('Manager', ))
+
+    def renderer(self, context=None, request=None, view=None, manager=None,
+                 assignment=None):
+        context = context or self.folder
+        request = request or self.folder.REQUEST
+        view = view or self.folder.restrictedTraverse('@@plone')
+        manager = manager or getUtility(
+            IPortletManager, name='plone.rightcolumn', context=self.portal)
+
+        assignment = assignment or collectionbysubject.Assignment(header='test header')
+        return getMultiAdapter((context, request, view, manager, assignment),
+                               IPortletRenderer)
+
+    def test_render(self):
+        r = self.renderer(context=self.portal,
+                          assignment=collectionbysubject.Assignment(header='test header'))
+        r = r.__of__(self.folder)
+        r.update()
+        output = r.render()
+        # TODO: Test output
+
+
+def test_suite():
+    from unittest import TestSuite, makeSuite
+    suite = TestSuite()
+    suite.addTest(makeSuite(TestPortlet))
+    suite.addTest(makeSuite(TestRenderer))
+    return suite
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
+
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='collective.portlet.collectionbysubject',
+      version=version,
+      description="Portlet that groups collections items by subject",
+      long_description=open("README.txt").read() + "\n" +
+                       open("HISTORY.txt").read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='zope plone portlet collective collection grouping',
+      author='Rok Garbas',
+      author_email='rok@garbas.si',
+      url='http://',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.portlet'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
