28acdb2f665d9482ef33107dde7b694d2ae4c733
pigeonflight <david@alteroo.com>
2008-11-01 14:42:23 +0000

    initial import
    git-svn-id: http://svn.plone.org/svn/collective/collective.portlet.collectionbysubject/trunk@74820 db7f04ef-aaf3-0310-a811-c281ed44c4ad

---
+++ collective/portlet/collectionbysubject/__init__.py
@@ -0,0 +1,5 @@
+from zope.i18nmessageid import MessageFactory
+CollectionBySubjectMessageFactory = MessageFactory('collective.portlet.collectionbysubject')
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
