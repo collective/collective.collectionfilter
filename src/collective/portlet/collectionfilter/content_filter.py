eed890e11eed4d37d728fc5e84454d97893a8030
Johannes Raggam <raggam-nl@adm.at>
2015-04-30 13:23:09 +0200

    subject does not accept unicode values

--- src/collective/portlet/collectionfilter/content_filter.py
+++ src/collective/portlet/collectionfilter/content_filter.py
@@ -1,4 +1,5 @@
 from .utils import safe_decode
+from .utils import safe_encode
 from .vocabularies import GROUPBY_CRITERIA
 from .vocabularies import TEXT_IDX
 
@@ -12,7 +13,12 @@ def set_content_filter(context, event):
     for val in GROUPBY_CRITERIA.values():
         idx = val['index']
         if idx in request.form:
-            content_filter[idx] = safe_decode(request.form.get(idx))
+            if idx == 'Subject':
+                # Subject does not accept unicode values
+                # https://github.com/plone/Products.CMFPlone/issues/470
+                content_filter[idx] = safe_encode(request.form.get(idx))
+            else:
+                content_filter[idx] = safe_decode(request.form.get(idx))
     if TEXT_IDX in request.form:
         content_filter[TEXT_IDX] = safe_decode(request.form.get(TEXT_IDX))
     event.request['contentFilter'] = content_filter
