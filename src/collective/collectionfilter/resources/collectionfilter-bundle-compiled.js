df59704ecb9c4ab6ef62d237a11b5bf2e1fbf5b6
Johannes Raggam <thetetet@gmail.com>
2016-12-21 20:50:17 -0600

    compiled

--- src/collective/collectionfilter/resources/collectionfilter-bundle-compiled.js
+++ src/collective/collectionfilter/resources/collectionfilter-bundle-compiled.js
@@ -157,43 +157,55 @@ define('collectionfilter',[
         },
 
         init: function() {
-            if (this.options.additive) {
-                this.$el.unbind('collectionfilter:reload');
-                this.$el.on('collectionfilter:reload', function (e, data) {
-                    if (data.collectionUUID === this.options.collectionUUID) {
-                        this.reload(data.targetFilterURL);
-                    }
-                }.bind(this));
-            }
-            $('.filteritem', this.$el).on('click', function (e) {
+            this.$el.unbind('collectionfilter:reload');
+            this.$el.on('collectionfilter:reload', function (e, data) {
+                if (data.collectionUUID === this.options.collectionUUID) {
+                    this.reload(data.targetFilterURL);
+                }
+            }.bind(this));
+
+            // OPTION 1 - filter rendered as links
+            $('a.filteritem', this.$el).on('click', function (e) {
                 e.stopPropagation();
                 e.preventDefault();
-                var collectionURL = e.target.closest('a').href  // dunno why, but e.target here is a span el, when it should be the a tag. this fixes it forward/backwards compatible...
+                var collectionURL = e.target.closest('a').href;  // strange, it's not the anchor element, but a span.
 
-                if (this.options.additive) {
-                    $(this.trigger).trigger(
-                        'collectionfilter:reload',
-                        {
-                            collectionUUID: this.options.collectionUUID,
-                            targetFilterURL: collectionURL
-                        }
-                    );
-                }
+                $(this.trigger).trigger(
+                    'collectionfilter:reload',
+                    {
+                        collectionUUID: this.options.collectionUUID,
+                        targetFilterURL: collectionURL
+                    }
+                );
 
                 this.reloadCollection(collectionURL + '&ajax_load=1');
             }.bind(this));
+
+            // OPTION 2 - filter rendered as checkboxes
+            $('input.filteritem', this.$el).on('change', function (e) {
+                var collectionURL = $(e.target).data('url');
+
+                $(this.trigger).trigger(
+                    'collectionfilter:reload',
+                    {
+                        collectionUUID: this.options.collectionUUID,
+                        targetFilterURL: collectionURL
+                    }
+                );
+
+                this.reloadCollection(collectionURL + '&ajax_load=1');
+            }.bind(this));
+
         },
 
         reload: function (filterURL) {
 
             var reloadURL = this.options.reloadURL;
-            if (this.options.additive) {
-                var urlParts = reloadURL.split('?');
-                var query1 = urlParts[1] || [];
-                var query2 = filterURL.split('?')[1] || [];
-                var query = [].concat(query1, query2).join('&');
-                var reloadURL = [].concat(urlParts[0], query).join('?');
-            }
+            var urlParts = reloadURL.split('?');
+            var query1 = urlParts[1] || [];
+            var query2 = filterURL.split('?')[1] || [];
+            var query = [].concat(query1, query2).join('&');
+            reloadURL = [].concat(urlParts[0], query).join('?');
 
             var cl = new this.contentloader(this.$el, {
                 url: reloadURL,
