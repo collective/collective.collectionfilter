1428e369adf79476b6a47ab6c860ed4339ff62a8
Dylan Jay <software@pretaweb.com>
2019-07-09 17:29:50 +0700

    fix install tests for 5.0.x

--- src/collective/collectionfilter/tests/test_setup.py
+++ src/collective/collectionfilter/tests/test_setup.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 """Setup tests for this package."""
+from Products.CMFCore.utils import getToolByName
+
 from collective.collectionfilter.testing import COLLECTIVE_COLLECTIONFILTER_INTEGRATION_TESTING  # noqa
 from plone import api
 from plone.app.testing import setRoles
@@ -12,57 +14,66 @@ no_get_installer = False
 try:
     from Products.CMFPlone.utils import get_installer
 except Exception:
-    no_get_installer = True
+    # Quick shim for 5.1 api change
 
-# TODO: fix tests for 5.0.x
-if not no_get_installer:
+    class get_installer(object):
+        def __init__(self, portal, request):
+            self.installer = getToolByName(portal, 'portal_quickinstaller')
 
-    class TestSetup(unittest.TestCase):
-        """Test that collective.collectionfilter is properly installed."""
+        def is_product_installed(self, name):
+            return self.installer.isProductInstalled(name)
 
-        layer = COLLECTIVE_COLLECTIONFILTER_INTEGRATION_TESTING
+        def uninstall_product(self, name):
+            return self.installer.uninstallProducts([name])
 
-        def setUp(self):
-            """Custom shared utility setup for tests."""
-            self.portal = self.layer['portal']
-            self.installer = get_installer(self.portal, self.layer['request'])
 
-        def test_product_installed(self):
-            """Test if collective.collectionfilter is installed."""
-            self.assertTrue(self.installer.is_product_installed(
-                'collective.collectionfilter'))
+class TestSetup(unittest.TestCase):
+    """Test that collective.collectionfilter is properly installed."""
 
-        def test_browserlayer(self):
-            """Test that ICollectionFilterBrowserLayer is registered."""
-            from collective.collectionfilter.interfaces import (
-                ICollectionFilterBrowserLayer)
-            from plone.browserlayer import utils
-            self.assertIn(
-                ICollectionFilterBrowserLayer,
-                utils.registered_layers())
+    layer = COLLECTIVE_COLLECTIONFILTER_INTEGRATION_TESTING
 
-    class TestUninstall(unittest.TestCase):
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        self.installer = get_installer(self.portal, self.layer['request'])
 
-        layer = COLLECTIVE_COLLECTIONFILTER_INTEGRATION_TESTING
+    def test_product_installed(self):
+        """Test if collective.collectionfilter is installed."""
+        self.assertTrue(self.installer.is_product_installed(
+            'collective.collectionfilter'))
 
-        def setUp(self):
-            self.portal = self.layer['portal']
-            self.installer = get_installer(self.portal, self.layer['request'])
-            roles_before = api.user.get_roles(TEST_USER_ID)
-            setRoles(self.portal, TEST_USER_ID, ['Manager'])
-            self.installer.uninstall_product('collective.collectionfilter')
-            setRoles(self.portal, TEST_USER_ID, roles_before)
+    def test_browserlayer(self):
+        """Test that ICollectionFilterBrowserLayer is registered."""
+        from collective.collectionfilter.interfaces import (
+            ICollectionFilterBrowserLayer)
+        from plone.browserlayer import utils
+        self.assertIn(
+            ICollectionFilterBrowserLayer,
+            utils.registered_layers())
 
-        def test_product_uninstalled(self):
-            """Test if collective.collectionfilter is cleanly uninstalled."""
-            self.assertFalse(self.installer.is_product_installed(
-                'collective.collectionfilter'))
 
-        def test_browserlayer_removed(self):
-            """Test that ICollectionFilterBrowserLayer is removed."""
-            from collective.collectionfilter.interfaces import \
-                ICollectionFilterBrowserLayer
-            from plone.browserlayer import utils
-            self.assertNotIn(
-                ICollectionFilterBrowserLayer,
-                utils.registered_layers())
+class TestUninstall(unittest.TestCase):
+
+    layer = COLLECTIVE_COLLECTIONFILTER_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.installer = get_installer(self.portal, self.layer['request'])
+        roles_before = api.user.get_roles(TEST_USER_ID)
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.installer.uninstall_product('collective.collectionfilter')
+        setRoles(self.portal, TEST_USER_ID, roles_before)
+
+    def test_product_uninstalled(self):
+        """Test if collective.collectionfilter is cleanly uninstalled."""
+        self.assertFalse(self.installer.is_product_installed(
+            'collective.collectionfilter'))
+
+    def test_browserlayer_removed(self):
+        """Test that ICollectionFilterBrowserLayer is removed."""
+        from collective.collectionfilter.interfaces import \
+            ICollectionFilterBrowserLayer
+        from plone.browserlayer import utils
+        self.assertNotIn(
+            ICollectionFilterBrowserLayer,
+            utils.registered_layers())
